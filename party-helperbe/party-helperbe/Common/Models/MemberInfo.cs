ee1b352f168c9c35e966574869c4c50b2f547123
HossamVahid <hossamvahid2@gmail.com>
2024-12-04 23:42:28 +0200

    Creating the feature to show members
    I created the feature to see all the members

---
+++ party-helperbe/party-helperbe/Common/Models/MemberInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace party_helperbe.Common.Models
+{
+    public class MemberInfo
+    {
+
+        public int memberId {  get; set; }
+
+        public string emailAddress { get; set; }
+        public string memberName { get; set; }
+    }
+}
---
+++ party-helperbe/party-helperbe/Controllers/MemberController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using party_helperbe.Common.Models;
+using party_helperbe.DataAccess.Models;
+
+namespace party_helperbe.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private readonly PgSQLDbContext _appData;
+
+        public MemberController(PgSQLDbContext appData)
+        {
+            _appData = appData;
+        }
+
+        [HttpGet("member/show/{page}")]
+        [Authorize(Roles ="Admin")]
+        public async Task<IActionResult> GetMemebers(int page = 1)
+        {
+
+            List<Member> memebers= await _appData.Members.ToListAsync();
+
+            var totalMembers = memebers.Count();
+
+            var totalPages = (int)Math.Ceiling((decimal)totalMembers / 5);
+
+            var membersOnPage = memebers.Skip((page - 1) * 5).Take(5).Select(m => new MemberInfo
+            {
+                memberId = m.memberId,
+                emailAddress = m.emailAddress,
+                memberName = m.userName
+            }).ToList();
+
+            return Ok(new {Page  = page, TotalPages=totalPages, Members =  membersOnPage});
+        } 
+
+
+    }
+}
